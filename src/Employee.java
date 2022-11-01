915d1aa1c0c99f1c7c3ca571afddcfec1145942a
Andrey-Legion <andrey.legion@gmail.com>
2022-11-01 15:13:30 +0300

    Курсовая работа 1 Черновик

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,61 @@
 public class Employee {
+    public static int id;
+    private final String firstName;
+    private final String secondName;
+    private final String thirdName;
+    private String department;
+    private double salary;
+
+    public Employee (int id,String firstName,String secondName,String thirdName,String department,double salary) {
+        this.id+=1;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.department = department;
+        this.salary = salary;
+    }
+    public static int getId () {
+        return id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getSecondName(){
+        return this.secondName;
+    }
+    public String getThirdName(){
+        return this.thirdName;
+    }
+    public String getDepartment(){
+        return this.department;
+    }
+    public void setDepartment(String department){
+        this.department=department;
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+    public void setSalary(double salary){
+        this.salary=salary;
+    }
+    public void printAllEmployees() {
+        for (int i = 0; i < id; i++) {
+            Employee employee = employee[i];
+            System.out.println(Employee.getId() + ": " + Employee.getFirstName() + Employee.getSecondName() + Employee.getThirdName());
+        }
+    }
+    public void printAllData() {
+        for (int i = 0; i < id; i++) {
+            Employee employee = employee[i];
+            System.out.println(Employee.getId() + ": " + Employee.employee());
+
+        }
+    }
+
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+
 public class Main {
     public static void main(String[] args) {
-
-
+        private Employee[] employee;
+        Employee[] employee= new Employee[10];
+        System.out.println(Employee.getId());
+        String[] department = new String[]{"1", "2", "3", "4", "5"};
+        Employee employee1 = new Employee("A1","B1","C1","2" ,1,1000d);
+        Employee employee2 = new Employee("A2","B2","C2","1" ,2,1500d);
+        /*person1.firstName = "A1";
+        person1.secondName = "B1";
+        person1.thirdName = "C1";
+        person1.department[]="2";
+        person1.salary=1000d;*/
+        System.out.println("Список сотрудников: "+Employee().getId());
+//        System.out.println("Затраты на зарплату в месяц: "+);
+//        System.out.println("Сотрудник с мин.зарплатой: "+);
+//        System.out.println("Сотрудник с макс.зарплатой: "+);
+//        System.out.println("Средняя зарплата: "+);
+        //System.out.println("Ф.И.О. сотрудников: "+employee().getFirstName()+employee().getSecondName()+employee().getThirdName());
+        Employee.printAllEmployees();
+        Employee.printAllData();
     }
 }
