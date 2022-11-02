1a9218051eef341da0f412496f90903bd0e899f7
Andrey-Legion <andrey.legion@gmail.com>
2022-11-02 21:05:28 +0300

    Курсовая работа 1 Черновик

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,14 @@
 public class Employee {
-    public static int id;
+    public static int id;               //будет меняться одновременно у всех.
+    static int quantityOfEmployees = 2; //лучше ввести доп поле,которое и будет хранить значение последнего ид
     private final String firstName;
     private final String secondName;
     private final String thirdName;
     private String department;
     private double salary;
 
-    public Employee (int id,String firstName,String secondName,String thirdName,String department,double salary) {
-        this.id+=1;
+    public Employee (String firstName,String secondName,String thirdName,String department,double salary) {
+        id++;
         this.firstName = firstName;
         this.secondName = secondName;
         this.thirdName = thirdName;
@@ -40,20 +41,17 @@ public class Employee {
     public void setSalary(double salary){
         this.salary=salary;
     }
-    public void printAllEmployees() {
-        for (int i = 0; i < id; i++) {
-            Employee employee = employee[i];
-            System.out.println(Employee.getId() + ": " + Employee.getFirstName() + Employee.getSecondName() + Employee.getThirdName());
-        }
-    }
-    public void printAllData() {
-        for (int i = 0; i < id; i++) {
-            Employee employee = employee[i];
-            System.out.println(Employee.getId() + ": " + Employee.employee());
 
-        }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", thirdName='" + thirdName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
     }
-
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        private Employee[] employee;
-        Employee[] employee= new Employee[10];
+        Employee[] person;
+        Employee[] person= new Employee[10];
         System.out.println(Employee.getId());
         String[] department = new String[]{"1", "2", "3", "4", "5"};
-        Employee employee1 = new Employee("A1","B1","C1","2" ,1,1000d);
-        Employee employee2 = new Employee("A2","B2","C2","1" ,2,1500d);
+        Employee person1 = new Employee("A1","B1","C1","2" ,1000d);
+        Employee person2 = new Employee("A2","B2","C2","1" ,1500d);
         /*person1.firstName = "A1";
         person1.secondName = "B1";
         person1.thirdName = "C1";
@@ -18,7 +18,19 @@ public class Main {
 //        System.out.println("Сотрудник с макс.зарплатой: "+);
 //        System.out.println("Средняя зарплата: "+);
         //System.out.println("Ф.И.О. сотрудников: "+employee().getFirstName()+employee().getSecondName()+employee().getThirdName());
-        Employee.printAllEmployees();
-        Employee.printAllData();
+
+        public void printAllEmployees() {
+            for (int i = 0; i < id; i++) {
+                Employee person = person[i];
+                System.out.println(Employee.getId() + ": " + Employee.getFirstName() + Employee.getSecondName() + Employee.getThirdName());
+            }
+        }
+        public void printAllData() {
+            for (int i = 0; i < id; i++) {
+                Employee person = person[i];
+                System.out.println(Employee.getId() + ": " + Employee.person());
+
+            }
+        }
     }
 }
